96dd164f58ae02259a91c2626eb6c29120cb0aea
SMHS-519-18 <SMHS-519-18@SMHS-519-18.smhsrop.lab>
2019-10-10 16:14:13 -0700

    LI Part 1

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,33 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class LeagueInvaders {
-
+	
+	JFrame frame;
+	final int w = 500;
+	final int h = 800;
+	
+	public static void main(String[] args) {
+		
+		LeagueInvaders game = new LeagueInvaders();
+		game.setup();
+		
+	}
+	
+	LeagueInvaders() {
+		
+		frame = new JFrame();
+		
+	}
+	
+	void setup() {
+		
+		JPanel panel = new JPanel();
+		
+		frame.add(panel);
+		frame.show();
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
 }
